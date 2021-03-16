a1939c3d10002735d9f95c2a431130d65d34770b
jbyuki <jyburkhard@hotmail.com>
2021-03-16 17:52:11 +0100

    fix output window
    Use winfixwidth and winfixheight to keep
    output window size when other windows are
    moved

--- lua/dash/init.lua
+++ lua/dash/init.lua
@@ -1,4 +1,4 @@
--- Generated from execute_buf.lua.tl, init.lua.tl, lua-quickfix.lua.tl, lua-test.lua.tl, python-test.lua.tl, python.lua.tl, resize.lua.tl, test_suite.lua.tl, title.lua.tl, vimscript-quickfix.lua.tl, vimscript-test.lua.tl, vimscript.lua.tl using ntangle.nvim
+-- Generated from init.lua.tl, resize.lua.tl using ntangle.nvim
 local output_lines = {}
 
 local execute_win, execute_buf
@@ -9,45 +9,7 @@ local previous
 
 local hl_ns
 
-local tests = {}
-
-local out_counter = 1
-tests["lua"] = {
-  str = [[
-print("hello")
-]],
-  expected = { "hello" }
-}
-tests["python"] = {
-  str = [[
-print("hello")
-]],
-  expected = { "hello", "" }
-}
-tests["vim"] = {
-  str = [[
-echo "hello"
-]],
-  expected = { "hello" }
-}
 local M = {}
-function M.execute_lines(lines, ft, show_pane, done)
-  local fname = vim.fn.tempname()
-  local f = io.open(fname, "w")
-  for _, line in ipairs(lines) do
-    f:write(line .. "\n")
-  end
-  f:close()
-  
-  local augmented = function()
-    os.remove(fname)
-    if done then
-      done()
-    end
-  end
-  M.execute(fname, ft, show_pane, augmented)
-end
-
 function M.execute(filename, ft, open_split, done)
   local buf
   if not execute_win or not vim.api.nvim_win_is_valid(execute_win) then
@@ -65,6 +27,8 @@ function M.execute(filename, ft, open_split, done)
     
     vim.api.nvim_command("bo " .. win_size .. split)
     execute_win = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_option(execute_win, "winfixheight", true)
+    vim.api.nvim_win_set_option(execute_win, "winfixwidth", true)
     
   end
   
@@ -75,18 +39,6 @@ function M.execute(filename, ft, open_split, done)
     vim.api.nvim_command("setlocal nonumber")
     vim.api.nvim_command("setlocal norelativenumber")
     execute_buf = vim.api.nvim_win_get_buf(0)
-    
-    local bufname
-    while true do
-      bufname = "Out #" .. out_counter
-      local oldbufnr = vim.fn.bufnr(bufname)
-      if oldbufnr == -1 then
-        break
-      end
-      out_counter = out_counter + 1
-    end
-    vim.api.nvim_buf_set_name(execute_buf, bufname)
-    out_counter = out_counter + 1
     vim.api.nvim_command("wincmd p")
     
     if previous then
@@ -100,9 +52,8 @@ function M.execute(filename, ft, open_split, done)
   
   local execute_win_height = vim.api.nvim_win_get_height(execute_win)
   
-  vim.api.nvim_command("cclose")
-  vim.fn.setqflist({})
   vim.api.nvim_win_set_height(execute_win, execute_win_height)
+  
 
   local stdin = vim.loop.new_pipe(false)
   local stdout = vim.loop.new_pipe(false)
@@ -197,19 +148,7 @@ function M.execute(filename, ft, open_split, done)
     	}, finish)
     
   elseif ft == "python" then
-    handle, err = vim.loop.spawn("python",
-    	{
-    		stdio = {stdin, stdout, stderr},
-    		args = {filename},
-    		cwd = ".",
-    	}, finish)
   elseif ft == "vim" then
-    handle, err = vim.loop.spawn("nvim",
-    	{
-    		stdio = {stdin, stdout, stderr},
-    		args = {"--headless", "-u", "NONE", "-c", "source " .. filename, "-c", "exit"},
-    		cwd = ".",
-    	}, finish)
   end
   assert(handle, err)
   
@@ -268,62 +207,6 @@ function M.execute(filename, ft, open_split, done)
           end
         end
         
-        if ft == "lua" then
-          for line in vim.gsplit(data, "\r*\n") do
-            if string.match(line, "^E%d+: Error while creating lua chunk: ") then
-              local errnum, fn, lnum, errmsg = string.match(line, "^E(%d+): Error while creating lua chunk: (.-%.lua):(%d+): (.*)")
-              
-              
-              vim.fn.setqflist({{
-                filename = fn, 
-                lnum = lnum, 
-                nr = errnum,
-                text = errmsg,
-                type = 'E'
-              }})
-              open_quickfix = true
-              
-            end
-          end
-        end
-        
-        if ft == "vim" then
-          local filename
-          local in_error = false
-          local lnum
-          local errors = {}
-          for line in vim.gsplit(data, "\r*\n") do
-            if string.match(line, "^Error detected while processing") then
-              filename = string.match(line, "^Error detected while processing (.+):")
-              
-              in_error = true
-            elseif in_error and string.match(line, "^line") then
-              lnum = string.match(line, "^line (%d+)")
-              
-            elseif in_error and string.match(line, "^E%d+: ") then
-              local errnum, errmsg = string.match(line, "^E(%d+): (.+)")
-              
-              table.insert(errors, {
-                filename = filename,
-                lnum = lnum,
-                nr = errnum,
-                text = errmsg,
-                type = 'E',
-              })
-              
-            end
-          end
-        
-          vim.fn.setqflist(errors)
-          if #errors > 0 then
-            open_quickfix = true
-          end
-        end
-        
-        if open_quickfix then
-          -- vim.api.nvim_command("copen")
-        end
-        
       end
     end)
   end)
@@ -350,67 +233,6 @@ function M.execute_buf()
   ft = vim.api.nvim_buf_get_option(0, "ft")
   
   M.execute(filename, ft, true)
-end
-
-function M.test()
-  local results = {}
-  for ft, test in pairs(tests) do
-    local done = false
-    M.execute_lines(vim.split(test.str, "\n"), ft, false, function()
-      done = true
-    end)
-    -- timeout 5 seconds
-    local ok = false
-    for i=1,100 do
-      vim.wait(50)
-      if done then
-        ok = true
-        break
-      end
-    end
-    
-    if not ok then
-      results[ft] = "TIMEOUT"
-    else
-      if vim.deep_equal(previous, test.expected) then
-        results[ft] = "OK"
-      else
-        results[ft] = "FAIL"
-      end
-    end
-    
-  end
-
-  local result_buf = vim.api.nvim_create_buf(false, true)
-  vim.api.nvim_win_set_buf(0, result_buf)
-  local lines = {}
-  local text = {}
-  local padding = 30
-  for ft,result in pairs(results) do
-    local line = ft
-    local s = string.len(ft)
-    for i=s+1,padding do
-      line = line .. " "
-    end
-    
-    line = line .. result
-    table.insert(lines, line)
-    table.insert(text, result)
-  end
-  vim.api.nvim_buf_set_lines(result_buf, 0, -1, true, lines)
-  local ns_id = vim.api.nvim_create_namespace("")
-  for i=1,#lines do
-    local hl_group
-    if text[i] == "OK" then
-      hl_group = "Search"
-    elseif text[i] == "TIMEOUT" then
-      hl_group = "Substitute"
-    else
-      hl_group = "IncSearch"
-    end
-    vim.api.nvim_buf_add_highlight(result_buf, ns_id, hl_group, i-1, padding, -1)
-  end
-  
 end
 
 
