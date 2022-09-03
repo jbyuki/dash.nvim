a09e06cf8b21cde5ffc91f0fdded1247a282c80b
jbyuki <jyburkhard@hotmail.com>
2022-09-03 22:05:51 +0200

    only parse cpp error if #output < 1000

--- lua/dash/init.lua
+++ lua/dash/init.lua
@@ -1478,28 +1478,30 @@ function M.execute(filename, ft, open_split, done)
                 execute_program()
               else
                 local error_lines, warning_lines = {}, {}
-                for _, line in ipairs(output_lines) do
-                  if (string.find(line, ": error") or string.find(line, ": fatal error")) then
-                    if string.find(line, "LNK") then
-                      local fn,error_str = string.match(line, "^%s*(.+):.+: (.+) %[")
-                      if fn and error_str then
-                        table.insert(error_lines, {fn, -1, error_str})
+                if #output_lines < 1000 then
+                  for _, line in ipairs(output_lines) do
+                    if (string.find(line, ": error") or string.find(line, ": fatal error")) then
+                      if string.find(line, "LNK") then
+                        local fn,error_str = string.match(line, "^%s*(.+):.+: (.+) %[")
+                        if fn and error_str then
+                          table.insert(error_lines, {fn, -1, error_str})
+                        end
+                      elseif not string.find(line, "^ ") then
+                        local fn,lnum,error_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
+                        if fn and lnum and error_str then
+                          table.insert(error_lines, {fn, lnum, error_str})
+                        end
                       end
-                    elseif not string.find(line, "^ ") then
-                      local fn,lnum,error_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
-                      if fn and lnum and error_str then
-                        table.insert(error_lines, {fn, lnum, error_str})
+                    elseif string.find(line, ": warning") and not string.find(line, "^ ") then
+                      local fn,lnum,warning_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
+                      if fn and lnum and warning_str then
+                        table.insert(warning_lines, {fn, lnum, warning_str})
                       end
+                    elseif string.find(line, "Warning%(s%)") then
+                      num_warnings = string.match(line, "(%d+)")
+                    elseif string.find(line, "Error%(s%)") then
+                      num_errors = string.match(line, "(%d+)")
                     end
-                  elseif string.find(line, ": warning") and not string.find(line, "^ ") then
-                    local fn,lnum,warning_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
-                    if fn and lnum and warning_str then
-                      table.insert(warning_lines, {fn, lnum, warning_str})
-                    end
-                  elseif string.find(line, "Warning%(s%)") then
-                    num_warnings = string.match(line, "(%d+)")
-                  elseif string.find(line, "Error%(s%)") then
-                    num_errors = string.match(line, "(%d+)")
                   end
                 end
 
--- src/cpp/vs_quickfix.lua.t
+++ src/cpp/vs_quickfix.lua.t
@@ -1,25 +1,27 @@
 ##../dash
 @parse_errors_cpp+=
 local error_lines, warning_lines = {}, {}
-for _, line in ipairs(output_lines) do
-  if (string.find(line, ": error") or string.find(line, ": fatal error")) then
-    if string.find(line, "LNK") then
-      @parse_linker_error
-    elseif not string.find(line, "^ ") then
-      local fn,lnum,error_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
-      if fn and lnum and error_str then
-        table.insert(error_lines, {fn, lnum, error_str})
+if #output_lines < 1000 then
+  for _, line in ipairs(output_lines) do
+    if (string.find(line, ": error") or string.find(line, ": fatal error")) then
+      if string.find(line, "LNK") then
+        @parse_linker_error
+      elseif not string.find(line, "^ ") then
+        local fn,lnum,error_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
+        if fn and lnum and error_str then
+          table.insert(error_lines, {fn, lnum, error_str})
+        end
       end
+    elseif string.find(line, ": warning") and not string.find(line, "^ ") then
+      local fn,lnum,warning_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
+      if fn and lnum and warning_str then
+        table.insert(warning_lines, {fn, lnum, warning_str})
+      end
+    elseif string.find(line, "Warning%(s%)") then
+      num_warnings = string.match(line, "(%d+)")
+    elseif string.find(line, "Error%(s%)") then
+      num_errors = string.match(line, "(%d+)")
     end
-  elseif string.find(line, ": warning") and not string.find(line, "^ ") then
-    local fn,lnum,warning_str = string.match(line, "^(.+)%((%d+),%d+%):[^:]+: (.+) %[")
-    if fn and lnum and warning_str then
-      table.insert(warning_lines, {fn, lnum, warning_str})
-    end
-  elseif string.find(line, "Warning%(s%)") then
-    num_warnings = string.match(line, "(%d+)")
-  elseif string.find(line, "Error%(s%)") then
-    num_errors = string.match(line, "(%d+)")
   end
 end
 
